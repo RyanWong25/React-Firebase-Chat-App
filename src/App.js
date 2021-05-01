15c334d8b1878ac819b8a13a5661cb4a00508576
Ryan Wong <rjowong25@gmail.com>
2021-04-30 21:39:02 -0400

    Initial setup in App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+// import firebase sdk, authentication, and firestore
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// import hooks for ease of use with firebase in React
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCHxzPsfdOPekN3_XIkpIpflwD5AOCfUTQ",
+  authDomain: "single-room-chat-app.firebaseapp.com",
+  projectId: "single-room-chat-app",
+  storageBucket: "single-room-chat-app.appspot.com",
+  messagingSenderId: "847548066294",
+  appId: "1:847548066294:web:4997b5680988760c8e3fd7",
+  measurementId: "G-WHDN9DTK31"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+
+  const [user] = userAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
